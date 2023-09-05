667d279c78a3fb490be46078530452e9f7ebd956
hholb <hholbroo@unca.edu>
2023-09-05 09:30:30 -0400

    add basic logging to HahVacuumAgentDriver

--- src/vacuumagentproject/HahVacuumAgentDriver.java
+++ src/vacuumagentproject/HahVacuumAgentDriver.java
@@ -1,55 +1,70 @@
 package vacuumagentproject;
 
 import java.util.Random;
+import java.util.logging.*;
 
 /**
- *
  * @author Hayden Holbrook
  */
 public class HahVacuumAgentDriver extends VacuumAgentDriver {
 
-    public static void main(String[] args) {
+  private static Logger logger = Logger.getLogger("logger");
 
-        //parse and set parameters using command line args
-        parseCommandLine(args);
-        if (setAgentLocRandomly) {
-            Random gen = new Random();
-            if (geometry == null) {
-                agentX = gen.nextInt(maxWidth);
-                agentY = gen.nextInt(maxHeight);
-            } else {
-                agentX = gen.nextInt(geometry[0].length());
-                agentY = gen.nextInt(geometry.length);
-            }
-        }
+  public static void main(String[] args) {
 
-        //initialize environment
-        VacuumEnvironment vEnviron;
+    FileHandler fh;
+
+    try {
+      fh = new FileHandler("sim.log", true);
+      logger.addHandler(fh);
+      logger.setLevel(Level.ALL);
+
+      // parse and set parameters using command line args
+      parseCommandLine(args);
+      if (setAgentLocRandomly) {
+        Random gen = new Random();
         if (geometry == null) {
-            vEnviron = new VacuumEnvironment(maxWidth, maxHeight, dirtProb, agent, agentX, agentY);
+          agentX = gen.nextInt(maxWidth);
+          agentY = gen.nextInt(maxHeight);
         } else {
-            vEnviron = new VacuumEnvironment(geometry, agent, agentX, agentY);
+          agentX = gen.nextInt(geometry[0].length());
+          agentY = gen.nextInt(geometry.length);
         }
+      }
 
-        if (useVisualizer) {
-            initialFloor = vEnviron.getFloor(false);
-        } else {
-            System.out.println("Initial Environment:");
-            System.out.println(vEnviron);
-        }
-        int timeSteps = vEnviron.simulate(maxTimeSteps, targetPerformance, usePercept);
-        trace = vEnviron.getTrace();
-        if (useVisualizer) {
-            System.out.println("Visualizer on");
-            visualize();
-        } else {
-            System.out.println("Final Environment:");
-            System.out.println(vEnviron);
+      // initialize environment
+      VacuumEnvironment vEnviron;
+      if (geometry == null) {
+        vEnviron = new VacuumEnvironment(maxWidth, maxHeight, dirtProb, agent, agentX, agentY);
+      } else {
+        vEnviron = new VacuumEnvironment(geometry, agent, agentX, agentY);
+      }
 
-            int count = 1;
-            for (VacuumTrace entry : trace) {
-                System.out.printf("%d: %s", count++, entry);
-            }
+      if (useVisualizer) {
+        initialFloor = vEnviron.getFloor(false);
+      } else {
+        System.out.println("Initial Environment:");
+        System.out.println(vEnviron);
+      }
+      int timeSteps = vEnviron.simulate(maxTimeSteps, targetPerformance, usePercept);
+      trace = vEnviron.getTrace();
+      if (useVisualizer) {
+        System.out.println("Visualizer on");
+        visualize();
+      } else {
+        System.out.println("Final Environment:");
+        System.out.println(vEnviron);
+
+        int count = 1;
+        for (VacuumTrace entry : trace) {
+          System.out.printf("%d: %s", count, entry);
+          logger.log(Level.INFO, String.format("%d: %s", count, entry));
+          count++;
         }
-    }//main    
+      }
+    } catch (Exception e) {
+      System.err.println("Something went wrong.");
+      System.exit(1);
+    }
+  } // main
 }
